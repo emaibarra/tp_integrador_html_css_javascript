dc26dc8caf61e5c9208333ea1d2e02c92bb8c6cc
emaibarra <ibarra.emanuel.022@gmail.com>
2024-10-17 12:49:41 -0300

    Fourth commit

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
+import { setLocalStorage } from "./src/persistence/localstorage";
 import { renderCategories } from "./src/services/categorias";
 import "./style.css";
 
 renderCategories();
+
+//Productos//
+const botonAgregarElemento = document.getElementById("botonAgregarElemento");
+
+botonAgregarElemento.addEventListener('click', () =>{
+ abrirPopUp();
+})
+
+
+
+//Popup//
+
+const botonCancelar = document.getElementById("botonCancelar")
+
+
+botonCancelar.addEventListener('click', ()=>{
+    cerrarPopUp();
+})
+
+
+//Funciones de abrir y cerrar popup //
+const abrirPopUp = () =>{
+    const popUpmain = document.getElementById("modalPopUp");
+    popUpmain.style.display = "flex";
+}
+const cerrarPopUp = () =>{
+    const popUpmain = document.getElementById("modalPopUp");
+    popUpmain.style.display = "none";
+}
+//guardar o modificar elementos
+
+const botonEliminar = document.getElementById("botonEliminar");
+const botonGuardar = document.getElementById("botonAgregar");
+
+botonGuardar.addEventListener('click', () => {
+    agregarEliminarElementos();
+})
+
+const agregarEliminarElementos = () =>{
+    const nombre = document.getElementById("nombre").value;
+    const imagen = document.getElementById("img").value;
+    const precio = document.getElementById("precio").value;
+    const categoria = document.getElementById("selectCategoria").value;
+
+    let object = {
+        id : new Date().toISOString(),
+        nombre,
+        imagen,
+        precio,
+        categoria,
+    }
+    setLocalStorage(object);
+    //cerrarPopUp();
+}
---
+++ src/persistence/localStorage.js
@@ -0,0 +1,32 @@
+// //LocalStorage// //
+
+
+//get de localStorage
+export const getLocalStorage = () => {
+    const productos = JSON.parse(localStorage.getItem("productos"));
+
+    if(productos){
+        return productos;
+    } else return [];
+}
+
+//guardar lcstg
+
+export const setLocalStorage = (productoIngresado) =>{
+    if(productoIngresado){
+    
+    let productosEnLocal = getLocalStorage();
+
+    const existingIndex = productosEnLocal.findIndex((productsLocal)=>
+        productsLocal.id === productoIngresado.id
+    )
+
+    if(existingIndex===!-1){
+        productosEnLocal[existingIndex] = productoIngresado;
+    } else{
+        productosEnLocal.push(productoIngresado);
+    }
+
+    localStorage.setItem("products", JSON.stringify(productosEnLocal));
+   }
+}
